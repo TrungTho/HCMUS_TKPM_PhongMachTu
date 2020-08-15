d70670ba80aa78f5826ba328d910f3694c193809
TrungTho <41388666+TrungTho@users.noreply.github.com>
2020-08-15 18:21:24 +0700

    add dangnhapvm

---
+++ HeThongPhongMachTu/HeThongPhongMachTu/ViewModels/DangNhapViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HeThongPhongMachTu.ViewModels
+{
+    public class DangNhapViewModel: BaseViewModel
+    {
+       
+            public bool Isloaded = false;
+            public ICommand UnitCommand { get; set; }
+
+
+            // mọi thứ xử lý sẽ nằm trong này
+            public DangNhapViewModel()
+            {
+                //UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                //    Isloaded = true;
+                //    var loginWindow = new DangNhap();
+                //    loginWindow.ShowDialog();
+                //}
+                //  );
+
+               // UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) => { UnitWindow wd = new UnitWindow(); wd.ShowDialog(); });
+
+
+                //MessageBox.Show(DataProvider.Ins.DB.Users.First().DisplayName);
+            }
+    }
+}
