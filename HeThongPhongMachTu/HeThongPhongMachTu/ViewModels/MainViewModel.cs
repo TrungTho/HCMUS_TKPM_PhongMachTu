be78599fde9e5d240a7700fa3957c90e3b613c95
TrungTho <41388666+TrungTho@users.noreply.github.com>
2020-08-15 17:15:08 +0700

    LoginWindowUI

--- HeThongPhongMachTu/HeThongPhongMachTu/ViewModels/MainViewModel.cs
+++ HeThongPhongMachTu/HeThongPhongMachTu/ViewModels/MainViewModel.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using DevExpress.Mvvm;
+using System.Windows;
 
 namespace HeThongPhongMachTu.ViewModels
 {
-    public class MainViewModel : ViewModelBase
+    public class MainViewModel : BaseViewModel
     {
+        public MainViewModel()
+        {
+            var screen = new DangNhap();
+            screen.ShowDialog();
+        }
     }
 }
