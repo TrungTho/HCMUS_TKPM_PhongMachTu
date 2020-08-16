24c2779873d47fe36e765efb0e3586c16219c8d4
TrungTho <41388666+TrungTho@users.noreply.github.com>
2020-08-16 14:44:06 +0700

    add ucTiepNhanDSTrongNgay & ucTiepNhan_DSBenhNhan

--- HeThongPhongMachTu/HeThongPhongMachTu/ViewModels/TiepNhan_DSBenhNhanViewModel.cs
+++ HeThongPhongMachTu/HeThongPhongMachTu/ViewModels/TiepNhan_DSBenhNhanViewModel.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.POCO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HeThongPhongMachTu.ViewModels
 {
     public class TiepNhan_DSBenhNhanViewModel:BaseViewModel
     {
+        private INavigationService NavigationService { get { return this.GetService<INavigationService>(); } }
+
+        public TiepNhan_DSBenhNhanViewModel() { }
+
+        public void NavigateDetails()
+        {
+            NavigationService.Navigate("ucTiepNhan_DSTrongNgay", null, this);
+        }
+
     }
 }
--- HeThongPhongMachTu/HeThongPhongMachTu/ViewModels/TiepNhan_DSTrongNgayViewModel.cs
+++ HeThongPhongMachTu/HeThongPhongMachTu/ViewModels/TiepNhan_DSTrongNgayViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.POCO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,13 @@ namespace HeThongPhongMachTu.ViewModels
 {
     public class TiepNhan_DSTrongNgayViewModel:BaseViewModel
     {
+        private INavigationService NavigationService { get { return this.GetService<INavigationService>(); } }
+
+        public TiepNhan_DSTrongNgayViewModel() { }
+
+        public void NavigateDetails()
+        {
+            NavigationService.Navigate("ucTiepNhan_DSBenhNhan", null, this);
+        }
     }
 }
