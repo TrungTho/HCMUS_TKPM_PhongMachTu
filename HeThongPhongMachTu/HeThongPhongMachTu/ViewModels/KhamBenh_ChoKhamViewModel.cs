7793534059b5839fa67c4b8deaf165b346527aa1
TrungTho <41388666+TrungTho@users.noreply.github.com>
2020-08-16 15:49:11 +0700

    add ucthanhtoan & uckhambenh
    6 uc

---
+++ HeThongPhongMachTu/HeThongPhongMachTu/ViewModels/KhamBenh_ChoKhamViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HeThongPhongMachTu.ViewModels
+{
+    public class KhamBenh_ChoKhamViewModel:BaseViewModel
+    {
+        #region command
+        #endregion
+
+        public ICommand ShowSomethingCommand { get; set; }
+
+        public KhamBenh_ChoKhamViewModel()
+        {
+            ShowSomethingCommand = new RelayCommand<object>((p) => { return true; }, (p) => { showSomething(); });
+        }
+
+        void showSomething()
+        {
+            MessageBox.Show("hahahaha");
+        }
+
+    }
+}
