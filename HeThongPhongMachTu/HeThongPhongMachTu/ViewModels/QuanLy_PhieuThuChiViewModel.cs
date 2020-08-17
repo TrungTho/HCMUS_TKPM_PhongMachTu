f009d08d9d93ac46f32d893a460318c171f5973f
buithianhthu99 <57214591+buithianhthu99@users.noreply.github.com>
2020-08-17 17:08:47 +0700

    Phieu TC

---
+++ HeThongPhongMachTu/HeThongPhongMachTu/ViewModels/QuanLy_PhieuThuChiViewModel.cs
@@ -0,0 +1,36 @@
+﻿using HeThongPhongMachTu.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeThongPhongMachTu.ViewModels
+{
+    public class QuanLy_PhieuThuChiViewModel : BaseViewModel
+    {
+        private ObservableCollection<PhieuThuChi> _listPhieuThuChi;
+
+        public ObservableCollection<PhieuThuChi> ListPhieuThuChi { get => _listPhieuThuChi; set { _listPhieuThuChi = value; OnPropertyChanged(); } }
+
+        public QuanLy_PhieuThuChiViewModel()
+        {
+            //itemsource for listview
+            ListPhieuThuChi = new ObservableCollection<PhieuThuChi>();
+
+            //select all record in phieuthuchi
+            var queries = DataProvider.Instance.DB.PhieuThuChis.ToList();
+
+            //modify some attribute to show in listview
+            int stt = 0;
+            foreach (var query in queries)
+            {
+                stt++;
+                PhieuThuChi phieuThuChi = new PhieuThuChi();
+                phieuThuChi = query;
+                ListPhieuThuChi.Add(phieuThuChi);
+            }
+        }
+    }
+}
