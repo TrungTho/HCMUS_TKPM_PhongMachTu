47251201714bca91d438609f37d6c91e36a8e611
TrungTho <41388666+TrungTho@users.noreply.github.com>
2020-08-17 17:49:42 +0700

    update combo QuanLy_ThongKe -
    add datepicker & binding to viewmodel

---
+++ HeThongPhongMachTu/HeThongPhongMachTu/ViewModels/QuanLy_ThongKeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace HeThongPhongMachTu.ViewModels
+{
+    public class QuanLy_ThongKeViewModel:BaseViewModel
+    {
+        public ICommand UpdateNgayBatDauCommand { get; set; }
+        public ICommand UpdateNgayKetThucCommand { get; set; }
+        public ICommand ShowSomeThing { get; set; }
+
+        private string ngayBatDau;
+        private string ngayKetThuc;
+
+        public string NgayBatDau { get => ngayBatDau; set { ngayBatDau = value; OnPropertyChanged(); } }
+        public string NgayKetThuc { get => ngayKetThuc; set { ngayKetThuc = value; OnPropertyChanged(); } }
+
+        public QuanLy_ThongKeViewModel()
+        {
+            UpdateNgayBatDauCommand = new RelayCommand<DatePicker>((p) => { return p == null ? false : true; }, (p) => NgayBatDau=p.Text);
+            UpdateNgayKetThucCommand = new RelayCommand<DatePicker>((p) => { return p == null ? false : true; }, (p) => NgayKetThuc=p.Text);
+            ShowSomeThing = new RelayCommand<object>((p) => { return  true; }, (p) => MessageBox.Show($"{NgayBatDau} - {ngayKetThuc}"));
+
+        }
+
+    }
+}
